00314b2b1eff3cf70a31b042df92eba2e1e5fb4e
Mehdi <azertyui20018@gmail.com>
2022-04-22 02:08:38 +0000

    completed js exercise number 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...num) {
+    let i = 0;
+    firstArray:
+    while (i < array.length) {
+        for (item of num) {
+            if (array[i] === item) { array.splice(i, 1); continue firstArray }
+        }
+        i++;
+    }
+    return array;
 };
 
 // Do not edit below this line
